445c42bd412ffebe6a1afcd53bc9d9667db65a14
Ellian <ellianalba1@gmail.com>
2022-06-15 11:13:49 -0400

    Add the functionality of set the color of the square

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
+let color = 'black'
 const BoardSet = (size) => {
     let board = document.querySelector('.board');
-    board.style.gridTemplateColumns = "repeat(16 , 1fr)";
-    board.style.gridTemplateRows = "repeat(16 , 1fr)";
+    let squares = board.querySelectorAll('div');
+    squares.forEach((div) => div.remove());
+    board.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size} , 1fr)`;
+    board.style.border = "1px solid black";
     
-    for (let i = 0; i < 256; i++) {
+    let amount = size * size;
+    for (let i = 0; i < amount; i++) {
         let square = document.createElement('div');
-        square.style.backgroundColor = 'blue';
+        square.addEventListener('mouseover', () => {
+            square.style.backgroundColor = color;
+        });
+        square.style.backgroundColor = 'whiite';
         board.insertAdjacentElement('beforeend', square);
     }
+};
+
+BoardSet();
+
+const SetSize = (input) => {
+    BoardSet(input);
+};
+
+const SetColor = (choice) => {
+    color = choice
 };
